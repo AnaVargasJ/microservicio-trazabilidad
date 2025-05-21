7598c197686c6bedbacb95bfbc30c22196d83b09
AnaVargasJ <anamariavargasj1590@gmail.com>
2025-05-21 08:28:50 -0500

    ADD: Creacion del modulo de microservicio trazabailidad

---
+++ src/test/java/com/avargas/devops/pruebas/app/microserviciotrazabilidad/MicroservicioTrazabilidadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.avargas.devops.pruebas.app.microserviciotrazabilidad;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicioTrazabilidadApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
